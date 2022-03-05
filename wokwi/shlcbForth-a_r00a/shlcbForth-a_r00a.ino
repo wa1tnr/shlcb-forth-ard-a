89b8ad8344bf6fe92f5215c524c910ebf94ecc42
wa1tnr <wa1tnr@users.noreply.github.com>
2022-03-05 13:01:34 +0000

    wokwi compliance

--- wokwi/shlcbForth-a_r00a/shlcbForth-a_r00a.ino
+++ wokwi/shlcbForth-a_r00a/shlcbForth-a_r00a.ino
@@ -1,3 +1,123 @@
-// Sat  5 Mar 12:44:23 UTC 2022
+// Sat  5 Mar 12:59:24 UTC 2022
 
-// empty sketch
+#include <serialStr.h>
+#include <strTools.h>
+
+#define blueLED   4
+#define purpleLED 3
+#define orangeLED 2
+
+serialStr  comReader;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("What light shall we turn on or off?");
+  pinMode(orangeLED,OUTPUT);
+  pinMode(purpleLED,OUTPUT);
+  pinMode(blueLED,OUTPUT);
+  comReader.setCallback(readStr);
+}
+
+void blue(bool on) {
+  if (on) {
+    digitalWrite(blueLED,HIGH);
+  } else {
+    digitalWrite(blueLED,LOW);
+  }
+}
+
+void purple(bool on) {
+  if (on) {
+    digitalWrite(purpleLED,HIGH);
+  } else {
+    digitalWrite(purpleLED,LOW);
+  }
+}
+
+void orange(bool on) {
+  if (on) {
+    digitalWrite(orangeLED,HIGH);
+  } else {
+    digitalWrite(orangeLED,LOW);
+  }
+}
+
+// Sat  5 Mar 10:50:44 UTC 2022
+
+void readStr(char* inStr) {
+    Serial.print("command : ");
+    Serial.println(inStr);
+    lwrCase(inStr);
+
+    if (!strcmp(inStr,"blue on"))    { blue(true);    return; }
+    if (!strcmp(inStr,"blue off"))   { blue(false);   return; }
+    if (!strcmp(inStr,"purple on"))  { purple(true);  return; }
+    if (!strcmp(inStr,"purple off")) { purple(false); return; }
+    if (!strcmp(inStr,"orange on"))  { orange(true);  return; }
+    if (!strcmp(inStr,"orange off")) { orange(false); return; }
+
+    if (!strcmp(inStr,"all on")) {
+        blue(true); purple(true); orange(true); return;
+    }
+    if (!strcmp(inStr,"all off")) {
+        blue(false); purple(false); orange(false); return;
+    }
+    // did not understand what was typed:
+    Serial.println("Look, its color on or color off.");
+    Serial.println("Or maybe, all on, or, all off. Try again.");
+}
+
+
+#if 0
+// Sat  5 Mar 10:28:12 UTC 2022
+
+void readStr(char* inStr) {
+    Serial.print("command : ");
+    Serial.println(inStr);
+    lwrCase(inStr);
+
+    if (!strcmp(inStr,"blue on")) {
+        blue(true); 
+        return;
+    }
+    if (!strcmp(inStr,"blue off")) {
+        blue(false);
+        return;
+    }
+    if (!strcmp(inStr,"purple on")) {
+        purple(true);
+        return;
+    }
+    if (!strcmp(inStr,"purple off")) {
+        purple(false);
+        return;
+    }
+    if (!strcmp(inStr,"orange on")) {
+        orange(true);
+        return;
+    }
+    if (!strcmp(inStr,"orange off")) {
+        orange(false);
+        return;
+    }
+    if (!strcmp(inStr,"all on")) {
+        blue(true);
+        purple(true);
+        orange(true);
+        return;
+    }
+    if (!strcmp(inStr,"all off")) {
+        blue(false);
+        purple(false);
+        orange(false);
+        return;
+    }
+    // did not understand what was typed:
+    Serial.println("Look, its color on or color off.");
+    Serial.println("Or maybe, all on, or, all off. Try again.");
+}
+
+#endif
+
+void loop( void ) { idle(); }
+// END.
